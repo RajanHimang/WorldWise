707b87ebea5fbaffb61454439cc9d5e706b3e9fe
Rajan Himang <rajanhimang@gmail.com>
2026-06-06 20:21:44 +1000

    Added Features

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useContext, useReducer } from "react";
+import { createContext, useEffect, useContext, useReducer, useCallback } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -83,7 +83,7 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
+ const getCity = useCallback( async function getCity(id) {
     
     if (Number(id) === currentCity.id) return;
 
@@ -98,7 +98,8 @@ function CitiesProvider({ children }) {
         payload: "There was an error loading city..",
       });
     }
-  }
+  }, [currentCity.id])
+  
   async function createCity(newCity) {
     dispatch({ type: "loading" });
     try {
